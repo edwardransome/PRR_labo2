43fa9da58e398d3d76fd8296d70f785e028eb943
Oceanos1 <michael.spierer@heig-vd.ch>
2017-12-06 19:30:18 +0100

    enlever les nombres magiques

--- travailleur/src/main/java/Main.java
+++ travailleur/src/main/java/Main.java
@@ -1,7 +1,7 @@
+import enums.Action;
 import remote.GestionnaireRMI;
 
 import java.rmi.Naming;
-import java.rmi.RMISecurityManager;
 import java.util.Scanner;
 
 
@@ -16,37 +16,37 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         try {
-            //System.setSecurityManager(new RMISecurityManager());
             GestionnaireRMI gest = (GestionnaireRMI) Naming.lookup("rmi://localhost/Gestionnaire" + id);
             while(!done){
-                System.out.println("Entrez 1 pour afficher la variable globale");
-                System.out.println("Entrez 2 pour modifier la variable globale");
-                System.out.println("Entrez 3 pour quitter");
+                System.out.println("Entrez "+ Action.CONSULT.getIndex() +" pour afficher la variable globale");
+                System.out.println("Entrez "+ Action.SET.getIndex() +" pour modifier la variable globale");
+                System.out.println("Entrez "+ Action.QUIT.getIndex() +" pour quitter");
                 int input = scanner.nextInt();
+                try {
+                    Action a = Action.values()[input-1];
 
-                switch (input){
-                    case 1:{
-                        int value = gest.consult();
-                        System.out.println("La valeur actuelle de la variable globale est de " + value);
-                        break;
+                    switch (a) {
+                        case CONSULT:
+                            int value = gest.consult();
+                            System.out.println("La valeur actuelle de la variable globale est de " + value);
+                            break;
+
+
+                        case SET:
+                            System.out.println("Entrez une nouvelle valeur pour la variable globale: ");
+                            int newValue = scanner.nextInt();
+                            gest.set(newValue);
+                            break;
+
+
+                        case QUIT:
+                            done = true;
+                            break;
+
                     }
-
-                    case 2:{
-                        System.out.println("Entrez une nouvelle valeur pour la variable globale: ");
-                        int newValue = scanner.nextInt();
-                        gest.set(newValue);
-                        break;
-                    }
-
-                    case 3:{
-                        done = true;
-                        break;
-                    }
-
-                    default:{
-                        System.out.println("Saisie incorrecte. Veuillez reessayer\n");
-                    }
-
+                }catch (IndexOutOfBoundsException e ){
+                    System.out.println("Saisie incorrecte. Veuillez reessayer\n");
+                    continue;
                 }
 
             }
---
+++ travailleur/src/main/java/enums/Action.java
@@ -0,0 +1,17 @@
+package enums;
+
+
+/**
+ * Enum déclarant les différents types d'actions que l'on peut faire dans le Main
+ *
+ * @author Edward Ransome
+ * @author Michael Spierer
+ */
+
+public enum Action {
+    CONSULT, SET, QUIT;
+
+    public int getIndex(){
+        return ordinal()+1;
+    }
+}
